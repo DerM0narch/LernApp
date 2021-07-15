8cde768f89df77178abbd10400fb199f7d3cf349
Patrick <p.lokau@web.de>
2021-07-15 08:44:46 +0200

    added Javadocs

--- src/klassen/Datenbank.java
+++ src/klassen/Datenbank.java
@@ -2,11 +2,18 @@ package klassen;
 
 import java.sql.*;
 
+/**
+ * Verbindung zur Datenbank mit Befehlen
+ */
 public class Datenbank {
     private Connection c;
     private Statement stmt;
     private final String URL = "jdbc:sqlite:res/fragenkatalog.db";
 
+    /**
+     * Befehl zum erstellen von Tabellen
+     * @param sql SQL Syntax verwenden (z.B. CREATE TABLE ...)
+     */
     public void execute(String sql) {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -22,6 +29,10 @@ public class Datenbank {
         }
     }
 
+    /**
+     * Befehl zum erstellen von Inhalten für Tabellen
+     * @param sql SQL Syntax verwenden (z.B. INSERT INTO ... VALUES ...)
+     */
     public void insert(String sql) {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -39,6 +50,13 @@ public class Datenbank {
         }
     }
 
+    /**
+     * Befehl zum auswählen von Inhalten in Tabellen
+     * @param sql SQL Syntax verwenden (z.B. SELECT ... FROM ...)
+     * @return ResultSet
+     * @throws SQLException Wenn Verbindung mit Datenbank misslingt
+     * @throws ClassNotFoundException wemm Statement nicht gefunden wird
+     */
     public ResultSet select(String sql) throws SQLException, ClassNotFoundException {
         ResultSet rs;
             Class.forName("org.sqlite.JDBC");
