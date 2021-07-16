fac7d5937860376bfc6b447fbc33d64243b93edc
Patrick <p.lokau@web.de>
2021-07-16 08:51:30 +0200

    select Befehle für jede Spalte

--- src/klassen/Datenbank.java
+++ src/klassen/Datenbank.java
@@ -12,6 +12,7 @@ public class Datenbank {
 
     /**
      * Befehl zum erstellen von Tabellen
+     *
      * @param sql SQL Syntax verwenden (z.B. CREATE TABLE ...)
      */
     public void execute(String sql) {
@@ -31,6 +32,7 @@ public class Datenbank {
 
     /**
      * Befehl zum erstellen von Inhalten für Tabellen
+     *
      * @param sql SQL Syntax verwenden (z.B. INSERT INTO ... VALUES ...)
      */
     public void insert(String sql) {
@@ -49,24 +51,141 @@ public class Datenbank {
     }
 
     /**
-     * Befehl zum auswählen von Inhalten in Tabellen
-     * @param sql SQL Syntax verwenden (z.B. SELECT ... FROM ...)
-     * @return ResultSet
-     * @throws SQLException Wenn Verbindung mit Datenbank misslingt
-     * @throws ClassNotFoundException wemm Statement nicht gefunden wird
+     * Select id as int.
+     *
+     * @param sql the sql statement
+     * @return the id
      */
-    public ResultSet select(String sql) throws SQLException, ClassNotFoundException {
-        ResultSet rs;
+    public int selectId(String sql) {
+        int id = 0;
+        try {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection(URL);
 
             stmt = c.createStatement();
-            rs = stmt.executeQuery(sql);
+            ResultSet rs = stmt.executeQuery(sql);
+            id = rs.getInt("id");
 
             rs.close();
             stmt.close();
             c.close();
+            return id;
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        return id;
+    }
 
-        return rs;
+    /**
+     * Select frage as string.
+     *
+     * @param sql the sql statement
+     * @return frage
+     */
+    public String selectFrage(String sql) {
+        String frage = "";
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection(URL);
+            c.setAutoCommit(false);
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            frage = rs.getString("frage");
+
+            rs.close();
+            stmt.close();
+            c.close();
+            return frage;
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        return frage;
+    }
+
+    /**
+     * Select richtig as string.
+     *
+     * @param sql the sql statement
+     * @return richtige Antwort
+     */
+    public String selectRichtig(String sql) {
+        String richtig = "";
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection(URL);
+            c.setAutoCommit(false);
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            richtig = rs.getString("richtig");
+
+            rs.close();
+            stmt.close();
+            c.close();
+            return richtig;
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        return richtig;
+    }
+
+    /**
+     * Select erste falsche Antwort as string.
+     *
+     * @param sql the sql statement
+     * @return erste falsche Antwort
+     */
+    public String selectErsteFalsch(String sql) {
+        String falsch = "";
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection(URL);
+            c.setAutoCommit(false);
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            falsch = rs.getString("ersteFalsch");
+
+            rs.close();
+            stmt.close();
+            c.close();
+            return falsch;
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        return falsch;
+    }
+
+    /**
+     * Select zweite falsche Antwort as string.
+     *
+     * @param sql the sql statement
+     * @return zweite falsche Antwort
+     */
+    public String selectZweiteFalsch(String sql) {
+        String falsch = "";
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection(URL);
+            c.setAutoCommit(false);
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            falsch = rs.getString("zweiteFalsch");
+
+            rs.close();
+            stmt.close();
+            c.close();
+            return falsch;
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        return falsch;
     }
 }
